629347da1ec08a07eb75cb13c147731f3d4ea4c7
Rimsha-Sanaullah <riimshasanaullah56@gmail.com>
2024-09-26 17:15:03 +0500

    Initial Structure

---
+++ src/components/ChartData.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ChartData = () => {
+  return (
+    <div>ChartData</div>
+  )
+}
+
+export default ChartData
---
+++ src/components/PreviousWeather.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PreviousWeather = () => {
+  return (
+    <div>PreviousWeather</div>
+  )
+}
+
+export default PreviousWeather;
